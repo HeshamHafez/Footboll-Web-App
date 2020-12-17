809ce2a10f6b7fb7f30f8735c2279535e754de9f
HeshamHafez <heshamhafez94@gmail.com>
2020-12-17 15:23:36 +0200

    rxjs smooth scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+
+   subscription : Subscription;
+  constructor(private router: Router) {
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.subscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(()=>window.scrollTo(0,0))
+  }
   title = 'footballwebapp';
 }
---
+++ src/app/dist/app.component.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppComponent = void 0;
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var operators_1 = require("rxjs/operators");
+var AppComponent = /** @class */ (function () {
+    function AppComponent(router) {
+        this.router = router;
+        this.title = 'footballwebapp';
+    }
+    AppComponent.prototype.ngOnDestroy = function () {
+        this.subscription.unsubscribe();
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.subscription = this.router.events.pipe(operators_1.filter(function (event) { return event instanceof router_1.NavigationEnd; })).subscribe(function () { return window.scrollTo(0, 0); });
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app-root',
+            templateUrl: './app.component.html',
+            styleUrls: ['./app.component.css']
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
